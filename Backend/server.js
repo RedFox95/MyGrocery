784cb8b6eb6ae1f216e4998543b4ad4b94dd378b
Ryan Weidling <ryanweidling@gmail.com>
2024-02-08 14:49:29 -0500

    added route for get request /

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // express app
 const app = express()
 
-// route handler
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// route handler for get to root directory
+// middleware example
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the MyGrocery home page'})
 })
 
 // listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000!')
+app.listen(process.env.PORT, () => {
+    console.log('listening on port',process.env.PORT)
 })
+
+process.env
